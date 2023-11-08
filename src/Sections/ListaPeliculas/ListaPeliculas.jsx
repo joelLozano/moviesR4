e9119ab5ae4c00facc2d4f7d9752aaed51525b64
Joel Lozano <joel.lozano@rappi.com>
2023-11-07 20:36:33 -0600

    loader

--- src/Sections/ListaPeliculas/ListaPeliculas.jsx
+++ src/Sections/ListaPeliculas/ListaPeliculas.jsx
@@ -1,72 +1,73 @@
 import { useState, useEffect} from 'react'
 import CardComponent from '../../Componets/Cards/Card'
+import { FaSpinner } from "react-icons/fa6";
+import './ListaPeliculas.css'
 
 export default function ListaPeliculas() {
 
     const [search, setSearch] = useState('')
     const [peliculas, setPeliculas] = useState([])
+    const [peliculasFilter, setPeliculasFilter] = useState([])
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
-        fetch('https://api-pelis-back.onrender.com/comedia')
-        .then(response => response.json())
-        .then(data => {
-            setPeliculas(data.peliculas)
-        })
-    }, [])
+      if (loading) {
+        fetch("https://api-pelis-back.onrender.com/comedia")
+          .then((response) => response.json())
+          .then((data) => {
+            setPeliculas(data.peliculas);
+            setPeliculasFilter(data.peliculas);
+            setLoading(false)
+          });
+      }
+    }, []);
 
     const handleSearch = (e) => {
-        //console.log(e.target.value)
-        setSearch(e.target.value)
+        console.log(e.target.value)
+        if (e.target.value == '') {
+            setPeliculasFilter(peliculas)
+        } else {
+            //console.log(e.target.value)
+        setSearch(e.target.value) // actualiza el estado de search - guarda lo que escribi 
+        let pelisFilter = peliculas.filter((peli) => peli.titulo.toLowerCase().includes(search.toLowerCase()))
+        setPeliculasFilter(pelisFilter)
+        }
     }
 
-    let pelis = [
-        {
-            title: 'Hombre araña y rasguña',
-            description: 'pelicula comica y de super herores',
-            portada: 'https://images5.alphacoders.com/132/thumbbig-1328421.webp'
-        },
-        {
-            title: 'El aro',
-            description: 'pelicula de terror',
-            portada: 'https://images5.alphacoders.com/132/thumbbig-1328421.webp'
-        }, 
-        {
-            title: 'Otra peli',
-            description: 'pelicula de terror',
-            portada: 'https://images5.alphacoders.com/132/thumbbig-1328421.webp'
-        },
-        {
-            title: 'Titanic',
-            description: 'pelicula de amor',
-            portada: 'https://images5.alphacoders.com/132/thumbbig-1328421.webp'
-        },
-        {
-            title: 'Titanic 2',
-            description: 'pelicula de amor 2',
-            portada: 'https://images5.alphacoders.com/132/thumbbig-1328421.webp'
-        }
-    ]
-
-  return (
-    <>
-    <div className="container">
-        <div className="row">
-            <div className="col d-flex justify-content-center">
-                <input type="text" placeholder='search' onChange={handleSearch}/>
-            </div>
-        </div>
-
-        <div className="row mt-5">
-            { pelis.map((item, index) => {
-                return (
-                    <div className="col-3 mt-5 mr-2" key={index}>
-                        <CardComponent portada={item.portada} title={item.title} description={item.description} />
+    if (loading) {
+        return (
+            <div className="container loader-container">
+                <div className="row">
+                    <div className="col ">
+                    <FaSpinner className='spinner'/>
                     </div>
-                )
-            })}
-        </div>
-    </div>
-    </>
-  )
+                </div>
+            </div>
+        )
+    } else {
+        return (
+            <>
+            <div className="container">
+                <div className="row">
+                    <div className="col d-flex justify-content-center">
+                        <input type="text" placeholder='search' onChange={handleSearch}/>
+                    </div>
+                </div>
+        
+                <div className="row mt-5">
+                    { peliculasFilter.map((item, index) => {
+                        return (
+                            <div className="col-3 mt-5 mr-2" key={index}>
+                                <CardComponent portada={item.portada} title={item.titulo} description={item.sinopsis} />
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+            </>
+          )
+    }
+
+  
 }
 
